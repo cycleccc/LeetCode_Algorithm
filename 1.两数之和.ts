172d223f6d81f542d6c25a61843c1afb0a3d5ee5
yaolongfei <2991205548@qq.com>
2024-11-14 17:21:53 +0800

    feat: add 两数只和

---
+++ 1.两数之和.ts
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=typescript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+    const hashMap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (hashMap.has(target - nums[i])) {
+            return [hashMap.get(target - nums[i]), i];
+        }
+        hashMap.set(nums[i], i);
+    }
+    return [];
+}
+// @lc code=end
