a3b7d0f95e4f1be1b64ecbd03de3c8f635ea22a6
yaolongfei <2991205548@qq.com>
2024-11-15 10:28:26 +0800

    feat: add palindromic numbers

---
+++ 9.回文数.ts
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=typescript
+ *
+ * [9] 回文数
+ */
+
+// @lc code=start
+function isPalindrome(x: number): boolean {
+    const s = x.toString();
+    for (let i = 0; i < s.length / 2; i++) {
+        if (s[i] !== s[s.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+// @lc code=end
